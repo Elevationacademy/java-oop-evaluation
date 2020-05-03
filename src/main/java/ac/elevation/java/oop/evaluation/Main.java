b3d4945a96ec8f1ed2e39b47c82e55c2679e29cf
Kfir Yehezkel <kfir.ye@gmail.com>
2020-05-03 17:54:30 +0300

    Fixes to adapt to evaluation testings.

--- src/main/java/ac/elevation/java/oop/evaluation/Main.java
+++ src/main/java/ac/elevation/java/oop/evaluation/Main.java
@@ -8,23 +8,25 @@ public class Main {
         Zoo zoo = new Zoo();
         ListIterator<Animal> zoo_iter = zoo.Iterator();
 
-        // ex1
-        zoo_iter.add(new Lion("Simba"));
-        //
-        //add more Animal to the Zoo.
-        // .
-        // .
-        // .
+        //ex1
+        // print all animals within the zoo.
+        // Simba - a lion
+        // Mufasa - a lion
+        // Rafiki - a monkey
+        // Akka - a goose
+        // Morten - a goose
+        // Hedwig - a snowy owl
+        // Toto - a dog
+        // Jaws - a shark
+        // Nemo - a clownfish
 
-        // ex2
-        zoo_iter.add(new Whale("Willy"));
-
-        // reset the iterator.
-        zoo_iter = zoo.Iterator();
         while (zoo_iter.hasNext()) {
             var a = zoo_iter.next();
             System.out.println(a.toString());
         }
+
+        // ex2
+        zoo_iter.add(new Whale("Wili");
 */
     }
 }
